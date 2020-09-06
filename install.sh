9b721a5b1680ec6ae45cf526e5a70d117b9f45ff
Vincent J. Margiotta <vmargiotta@iseatz.com>
2020-09-06 08:47:20 -0500

    add comments to install script

--- install.sh
+++ install.sh
@@ -2,10 +2,27 @@
 
 source bootstrap.sh
 
-# __check_for_previous_installation
+# Fail early if the system has been previously bootstrapped
+__check_for_previous_installation
+
+# In order to bootstrap homebrew, we need cURL, ruby, and git
+# to be installed:
 __verify_dependencies
-# __install_homebrew
-# __homebrew_install_formulae
-# __homebrew_install_casks
-# __install_oh_my_zsh
-# __complete_installation
+
+# Verify the Homebrew installation before continuing
+__check_homebrew
+__homebrew_install_formulae
+__homebrew_install_casks
+
+# Install Oh-My-Zsh
+__install_oh_my_zsh
+
+# Verify the Vim installation before continuing
+__check_vim
+__install_vundle
+__vundle_install_plugins
+
+# Finally, touch a simple text file to indicate the installation
+# was a success ($HOME/.installed). Delete this file to reiterate
+# the process.
+__complete_installation
