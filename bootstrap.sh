fec267653dbba3d5c49a1744f346c4f4a187a825
Vincent J. Margiotta <vim112358@gmail.com>
2020-09-11 11:15:28 -0500

    document homebrew functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,18 +16,23 @@ __check_for_previous_installation() {
 
 ##
 # Dependencies & executables
+
+# Detect and output the path of the `homebrew` binary or outputs a friendly message.
 __check_git() {
   command -v git || { echo "Dependency not found: git"; exit 1; }
 }
 
+# Detect and output the path of the `homebrew` binary or outputs a friendly message.
 __check_ruby() {
   command -v ruby || { echo "Dependency not found: ruby"; exit 1; }
 }
 
+# Detect and output the path of the `homebrew` binary or outputs a friendly message.
 __check_curl() {
   command -v curl || { echo "Dependency not found: curl"; exit 1; }
 }
 
+# Exit with error code `1` unless all dependencies are satisfied.
 __verify_dependencies() {
   __check_git && __check_ruby && __check_curl
 }
@@ -59,13 +64,18 @@ __git_clone() {
 
 
 ##
-# Homebrew
+# Homebrew - Package manager for macos.
+#
+# Offers several library functions to accomplish install homebrew itself plus
+# its notion of packages-- formulae and casks-- from files.
 HOMEBREW_BOOTSTRAP_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
 
+# Detect and output the path of the `homebrew` binary or outputs a friendly message.
 __check_homebrew() {
   command -v brew || { echo "Install: homebrew"; __install_homebrew; }
 }
 
+# References the homebrew binary by its absolute path.
 __HOMEBREW() {
   command brew "$@"
 }
@@ -78,28 +88,41 @@ __install_homebrew() {
   __ruby_script "$( __curl_url "$HOMEBREW_BOOTSTRAP_URL" )"
 }
 
+# Homebew install a formula by name.
+# Ignores any formula that starts with '#'. Supports multiple formulae per line.
 __homebrew_install_formula() {
-  ( [ -z "$1" ] && return ) || __HOMEBREW install "$1"
+  # Detect and bail on comments
+  [[ "$(echo "$@" | cut -c1)" == "#" ]] && return
+  ( [ "$#" -eq 0 ] && return ) || __HOMEBREW install "$@"
 }
 
+# Homebrew install all formulae found in $FORMULAE_FILE.
+# Supports both one-per-line and single line specifications.
+# If no filename is provided, defaults to `formulae.txt`.
 __homebrew_install_formulae() {
   FORMULAE_FILE="${1:-formulae.txt}"
 
   while read -r FORMULA; do
     __homebrew_install_formula "$FORMULA"
-  done <<< "$( cat "$FORMULAE_FILE" )"
+  done <<< "$(cat "$FORMULAE_FILE")"
 }
 
+# Homebrew install a cask by name.
+# Ignores any cask that starts with '#'. Supports multiple casks per line.
 __homebrew_install_cask() {
-  ( [ -z "$1" ] && return ) || __HOMEBREW cask install "$1"
+  [[ "$(echo "$@" | cut -c1)" == "#" ]] && return
+  ( [ "$#" -eq 0 ] && return ) || __HOMEBREW cask install "$@"
 }
 
+# Homebrew install all casks found in "$CASKS_FILE".
+# Supports both one-per-line and single line specifications.
+# If no filename is provided, defaults to `casks.txt`.
 __homebrew_install_casks() {
   CASKS_FILE="${1:-casks.txt}"
 
   while read -r CASK; do
     __homebrew_install_cask "$CASK"
-  done <<< "$( cat "$CASKS_FILE" )"
+  done <<< "$(cat "$CASKS_FILE")"
 }
 
 
